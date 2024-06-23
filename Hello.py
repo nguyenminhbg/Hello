9a0f5d859e0ced877bcaf035d56f0670d3bc3ec4
DESKTOP-TS0SLKR\minhd <minhdt3.k9@gmail.com>
2024-06-23 18:01:33 +0700

    Initial commit

---
+++ Hello.py
@@ -0,0 +1,25 @@
+import tkinter as tk
+from Main import greet, method2
+root = tk.Tk()
+root.title("The simple UI")
+root.geometry('500x500')
+root['bg']='gray'
+label=tk.Label(root,text="Hello, this is sample example")
+label.place(x=250,y=250)
+count = 1
+def on_button_click():
+    global count
+    print(count)
+    label.config(text="You already clicked on button")
+    if count%2==0:
+        label.config(text="The content is empty")
+    else:
+        label.config(text="The label content is changed")
+    count +=1
+    greet("Minh Nguyen")
+    method2(35)
+    label1=tk.Label(root,text="Add more")
+    label1.pack()
+button=tk.Button(root,text="Click here", command=on_button_click)
+button.pack(pady=20)
+root.mainloop()
---
+++ Main.py
@@ -0,0 +1,4 @@
+def greet(name):
+    print(f"Xin chao: {name}")
+def method2(age):
+    print(age)
